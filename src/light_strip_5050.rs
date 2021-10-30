6398bad0cc07ab4c7dd222a0e5f87adcb98edd94
Zach Nielsen <zach@zachn.me>
2021-10-29 17:03:19 -0700

    Adding some implementation for the 5050. Everything compiling now

--- src/light_strip_5050.rs
+++ src/light_strip_5050.rs
@@ -1,5 +1,8 @@
 // Copyright © Zach Nielsen 2021
 
+// The 5050 manager works by setting a brightness ratio for each color,
+// multiplied by the requested brightness.
+
 use crate::PresetColor;
 use crate::LedStrip;
 
@@ -25,62 +28,74 @@ const PWM_PERIOD: u16 = 100;
 const PWM_UPDATE: u16 = 1;
 struct PinPwmPair {
     pin: gpio::sysfs::SysFsGpioOutput,
-    pub duty: u16,
+    pub ratio: f32,
 }
 
 pub struct LightStrip5050 {
-    multi_led_info: Arc<Mutex<HashMap<LedPin, PinPwmPair>>>,
+    multi_led_info: HashMap<LedPin, PinPwmPair>,
+    brightness: u16,
 }
 impl LightStrip5050 {
     pub fn new() -> LightStrip5050 {
         let mut default_map = HashMap::new();
-        default_map.insert(LedPin::Red,   PinPwmPair{ pin: gpio::sysfs::SysFsGpioOutput::open(RED_PIN).unwrap(),   duty: 0 });
-        default_map.insert(LedPin::Green, PinPwmPair{ pin: gpio::sysfs::SysFsGpioOutput::open(GREEN_PIN).unwrap(), duty: 0 });
-        default_map.insert(LedPin::Blue,  PinPwmPair{ pin: gpio::sysfs::SysFsGpioOutput::open(BLUE_PIN).unwrap(),  duty: 0 });
+        default_map.insert(LedPin::Red,   PinPwmPair{ pin: gpio::sysfs::SysFsGpioOutput::open(RED_PIN).unwrap(),   ratio: 1.0 });
+        default_map.insert(LedPin::Green, PinPwmPair{ pin: gpio::sysfs::SysFsGpioOutput::open(GREEN_PIN).unwrap(), ratio: 1.0 });
+        default_map.insert(LedPin::Blue,  PinPwmPair{ pin: gpio::sysfs::SysFsGpioOutput::open(BLUE_PIN).unwrap(),  ratio: 1.0 });
 
         LightStrip5050 {
-            multi_led_info: Arc::new(Mutex::new(default_map)),
+            multi_led_info: default_map,
+            brightness: 0,
         }
     }
 }
 impl LedStrip for LightStrip5050 {
-    fn set_brightness(&self, brightness: u16) {
+    fn set_brightness(&mut self, brightness: u16) {
     }
 
-    fn set_color(&self, color: PresetColor) {
-        // match color {
-        //     PresetColor::Red:
-        //     PresetColor::Green:
-        //     PresetColor::Blue:
-        // }
+    fn set_color(&mut self, color: PresetColor) {
+        self.multi_led_info.get_mut(&LedPin::Red).unwrap().ratio = 0.0;
+        self.multi_led_info.get_mut(&LedPin::Green).unwrap().ratio = 0.0;
+        self.multi_led_info.get_mut(&LedPin::Blue).unwrap().ratio = 0.0;
+        match color {
+            PresetColor::Red   => { self.multi_led_info.get_mut(&LedPin::Red).unwrap().ratio   = 1.0; }
+            PresetColor::Green => { self.multi_led_info.get_mut(&LedPin::Green).unwrap().ratio = 1.0; }
+            PresetColor::Blue  => { self.multi_led_info.get_mut(&LedPin::Blue).unwrap().ratio  = 1.0; }
+        }
     }
 
-    fn set_rgb(&self, red_val: u8, green_val: u8, blue_val: u8) {
+    fn set_rgb(&mut self, red_val: u8, green_val: u8, blue_val: u8) {
     }
+}
 
-    fn init(&self) {
+pub struct LightStrip5050Manager {
+    pub lights: Arc<Mutex<LightStrip5050>>
+}
+impl LightStrip5050Manager {
+    pub fn new() -> LightStrip5050Manager { LightStrip5050Manager{ lights: Arc::new(Mutex::new(LightStrip5050::new())) } }
+
+    pub fn init(&self) {
         // Launch a thread to manage PWM
-        let mut thread_leds = Arc::clone(&self.multi_led_info);
+        let thread_lights = Arc::clone(&self.lights);
         let mut counter: u16 = 0;
         thread::spawn(move || loop {
             thread::sleep(std::time::Duration::from_millis(PWM_UPDATE as u64));
-            let mut leds = thread_leds.lock().unwrap();
+            let mut lights = thread_lights.lock().unwrap();
 
             // TODO - increment based on actual time elapsed, rather than nominal time waited
             counter = (counter + PWM_UPDATE) % PWM_PERIOD;
 
             for color in vec![LedPin::Red, LedPin::Green, LedPin::Blue].into_iter(){
-                if counter <= leds[&color].duty {
+                if counter <= lights.multi_led_info[&color].ratio as u16 * lights.brightness {
                     // Turn on
-                    leds.get_mut(&color).unwrap().pin.set_high().unwrap();
+                    lights.multi_led_info.get_mut(&color).unwrap().pin.set_high().unwrap();
                 }
                 else {
                     // Turn off
-                    leds.get_mut(&color).unwrap().pin.set_low().unwrap();
+                    lights.multi_led_info.get_mut(&color).unwrap().pin.set_low().unwrap();
                 }
             }
 
-            drop(leds);
+            drop(lights);
         });
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,12 @@
 // TODO - add readme explaining that this is supposed to hook into Shortcuts
 
 mod light_strip_5050;
-use crate::light_strip_5050::LightStrip5050;
+use crate::light_strip_5050::LightStrip5050Manager;
 
 #[macro_use] extern crate rocket;
 
 use rocket::request::FromParam;
 use rocket::State;
-
-use gpio::GpioOut;
 
 pub enum PresetColor {
     Red,
@@ -30,36 +28,40 @@ impl<'r> FromParam<'r> for PresetColor {
 }
 
 pub trait LedStrip {
-    fn set_brightness(&self, brightness: u16);
-    fn set_color(&self, color: PresetColor);
+    fn set_brightness(&mut self, brightness: u16);
+    fn set_color(&mut self, color: PresetColor);
     // Values are 0-255
-    fn set_rgb(&self, red_val: u8, green_val: u8, blue_val: u8);
-    fn init(&self);
+    fn set_rgb(&mut self, red_val: u8, green_val: u8, blue_val: u8);
 }
-// pub struct LightStrip<T: LedStrip> (pub T);
 
-// #[post("/set_color/<color>")]
-// fn preset_color_handler(config: &State<LightStrip>, color: PresetColor) {
-
-// }
-
-// #[post("/set_color_rgb/<red_val>/<green_val>/<blue_val>")]
-// fn ad_hoc_color_handler(config: &State<LightStrip>, red_val: u16, green_val: u16, blue_val: u16) {
-// }
-
-// #[post("/set_duty_all/<brightness>")]
-// fn brightness_handler(config: &State<LightStrip>, brightness: u16) {
-// }
+///////////////////////////////////////////////////////////////////////////////
+// 5050 Handlers
+///////////////////////////////////////////////////////////////////////////////
+#[post("/set_color/<color>")]
+fn preset_color_handler(manager: &State<LightStrip5050Manager>, color: PresetColor) {
+    let mut lock = manager.lights.lock().unwrap();
+    lock.set_color(color);
+}
+#[post("/set_color_rgb/<red_val>/<green_val>/<blue_val>")]
+fn ad_hoc_color_handler(manager: &State<LightStrip5050Manager>, red_val: u8, green_val: u8, blue_val: u8) {
+    let mut lock = manager.lights.lock().unwrap();
+    lock.set_rgb(red_val, green_val, blue_val);
+}
+#[post("/set_duty_all/<brightness>")]
+fn brightness_handler(manager: &State<LightStrip5050Manager>, brightness: u16) {
+    let mut lock = manager.lights.lock().unwrap();
+    lock.set_brightness(brightness);
+}
 
 #[launch]
 fn rocket() -> _ {
-    let lights = LightStrip5050::new();
-    lights.init();
+    let light_manager = LightStrip5050Manager::new();
+    light_manager.init();
 
     rocket::build()
-        // .manage(LightStrip(lights))
-        // .mount("/", routes![preset_color_handler])
-        // .mount("/", routes![ad_hoc_color_handler])
-        // .mount("/", routes![brightness_handler])
+        .manage(light_manager)
+        .mount("/", routes![preset_color_handler])
+        .mount("/", routes![ad_hoc_color_handler])
+        .mount("/", routes![brightness_handler])
 }
 
